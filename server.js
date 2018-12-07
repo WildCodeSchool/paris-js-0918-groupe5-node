7a22288c7d0eaf0b00998d807bb0fa979e2c7026
Elisa <elucchini17@gmail.com>
2018-12-07 18:35:21 +0100

    post route created

--- server.js
+++ server.js
@@ -2,29 +2,38 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const models = require("./models"); 
-app.use(cors())
+const models = require("./models");
+const routerContacts = require('./routes/contact');
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(
- bodyParser.urlencoded({
-   extended: true
- })
+  bodyParser.urlencoded({
+    extended: true
+  })
 );
+app.use('/contacts', routerContacts);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.post("/api", (req, res) => {
-    const newUser = new models.user({
-      lastName: req.body.lastName,
-      firstName: req.body.firstName
-    });
-    newUser.save();
-    res.sendStatus(200);
-   });
+  const newUser = new models.user({
+    lastName: req.body.lastName,
+    firstName: req.body.firstName
+  });
+  newUser.save();
+  res.sendStatus(200);
+});
 
-app.get('/api', (req, res) => models.user.findAll({}).then(user => res.json(user)))
+
+app.get("/api", (req, res) =>
+  models.user.findAll({}).then(user => res.json(user))
+);
+
+
 
 const port = process.env.PORT || 4243;
+
 models.sequelize.sync().then(() => {
- app.listen(port, () => console.log(`Listening on port ${port}...`));
+  app.listen(port, () => console.log(`Listening on port ${port}...`));
 });
