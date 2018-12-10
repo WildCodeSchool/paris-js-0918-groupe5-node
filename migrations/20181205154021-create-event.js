2a6e700fd1f0082b726344eecc0afb9a19630ae2
Elisa <elucchini17@gmail.com>
2018-12-10 13:28:05 +0100

    merging old node repo and new node repo

---
+++ migrations/20181205154021-create-event.js
@@ -0,0 +1,66 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('events', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      dateBeginning: {
+        type: Sequelize.DATE
+      },
+      dateEnd: {
+        type: Sequelize.DATE
+      },
+      hourBeginning: {
+        type: Sequelize.TIME
+      },
+      hourEnd: {
+        type: Sequelize.TIME
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      frequency: {
+        type: Sequelize.STRING
+      },
+      accountable: {
+        type: Sequelize.STRING
+      },
+      visibility: {
+        type: Sequelize.BOOLEAN
+      },
+      recall: {
+        type: Sequelize.BOOLEAN
+      },
+      immediateRecall: {
+        type: Sequelize.BOOLEAN
+      },
+      mood: {
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('events');
+  }
+};
