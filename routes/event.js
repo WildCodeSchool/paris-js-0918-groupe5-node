4a76bd0d9a093ed1453135d1d609ae39c6cbdc53
Widaad Barreto <widaad.barreto@gmail.com>
2018-12-12 17:25:23 +0100

    routes event

--- routes/event.js
+++ routes/event.js
@@ -9,4 +9,19 @@ router.get('/', (req,res) => {
 	});
 });
 
+router.post('/', (req,res) => {
+	const data = req.body;
+	console.log(req)
+	console.log(`Ajout de ${data}`);
+	const newEvent = new models.event(data);
+	newEvent.save()
+		.then(newEvent => {
+			// when we received a newContact, we send back a JSON to the client
+			res.status(200).json(newEvent)
+        })
+        .catch(err => {
+			console.log(err.message);
+        });
+});
+
 module.exports = router;
