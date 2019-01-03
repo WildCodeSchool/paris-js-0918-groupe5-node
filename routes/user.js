4399e098f0fed60aa2d23b8aee4e9d5911de466e
Louty <louty@Carolinas-Air.localdomain>
2019-01-03 16:31:46 +0100

    add router get

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const models = require('../models');
 
+router.get('/', (req,res) => {
+	models.users.findAll()
+	.then(data => {
+		res.status(200).json(data)
+	});
+});
+
 
 router.post('/', (req,res) => {
 	const data = req.body;
