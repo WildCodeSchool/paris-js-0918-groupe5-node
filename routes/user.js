41af0a18ace81d967f9baad336ec0230c6f34a61
Widaad Barreto <widaad.barreto@gmail.com>
2019-01-09 15:14:58 +0100

    post and get of contacts in fonction of receiver are ok

--- routes/user.js
+++ routes/user.js
@@ -20,73 +20,100 @@ router.route('/events')
       if (err) {
         res.sendStatus(403);
       } else {
-          const newEvent = {
-            ...req.body,
-          };
-          models.Event.create(newEvent)
-            .then((event) => {
-              models.User.findById(decode.id)
-                .then((caregiver) => {
-                  caregiver.getReceiver()
-                    .then((receivers) => {
-                      receivers[0].addEvent(event)
-                        .then((eventCreated) => { res.status(200).json(eventCreated); });
-                });
-            });
+        const newEvent = {
+          ...req.body,
+        };
+        models.Event.create(newEvent) // on créée un nouvel event dans la table Event avec le contenu du req.body
+          .then((event) => {
+            models.User.findById(decode.id) // ensuite on va chercher le caregiver dont l'id correspond à celui du token
+              .then((caregiver) => {
+                caregiver.getReceiver() // je récupère les receiver du caregiver
+                  .then((receivers) => {
+                    receivers[0].addEvent(event) // et je lie l'événement créée au 1er receiver
+                      .then((eventCreated) => {
+                        res.status(200).json(eventCreated);
+                      });
+                  });
+              });
           });
       }
     });
   })
   .get((req, res) => { // pour récupérer les events d'un user une fois connecté
-  const token = getToken(req); // on utilise la fonction créée dans getToken pour récupérer le token (clé créée lors du signin) qui identifie le user
-  jwt.verify(token, jwtSecret, (err, decode) => { // decode c'est ce qu'il y a dans le tokenInfo (donc l'id)
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      // models.User.findAll({
-      //   limit: 1,
-      //   where: {
-      //     userId: decode.id,
-      //     status: true,
-      //   },
-      //   order: [['createdAt', 'DESC']],
-      // }).then(result=>console.log("receiverId", result))
-      models.User.findById(decode.id).then((caregiver) => { // on checke que l'id dans le token corresponde à la foreign key de user qui est dans event pour avoir les event d'un user en particulier
-        caregiver.getReceiver().then((receivers) => { // méthode de sequelize pour récupérer les receivers du caregiver identifié
-          // console.log('===================receivers', receivers);
-          // const idReceiversArr = receivers.map((receiver) => {
-          //   return receiver.dataValues.id;
-          // }).sort();
-          // console.log(idReceiversArr[0]);
-          receivers[0].getEvents().then((events) => { // on récupére le 1er receiver (le plus ancien créé) et je récupére ses events avec la méthode getEvents de sequelize
-            res.status(200).json(events);
+    const token = getToken(req); // on utilise la fonction créée dans getToken pour récupérer le token (clé créée lors du signin) qui identifie le user
+    jwt.verify(token, jwtSecret, (err, decode) => { // decode c'est ce qu'il y a dans le tokenInfo (donc l'id)
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        models.User.findById(decode.id) // on checke que l'id dans le token corresponde à la foreign key de user qui est dans event pour avoir les event d'un user en particulier
+        .then((caregiver) => {
+          caregiver.getReceiver() // méthode de sequelize pour récupérer les receivers du caregiver identifié
+            .then((receivers) => {
+              if (receivers[0].status) { // si le receiver le plus ancien est actif
+                receivers[0].getEvents() // on récupére le 1er receiver (le plus ancien créé) et je récupére ses events avec la méthode getEvents de sequelize
+                  .then((events) => {
+                    res.status(200).json(events);
+                });
+              }
           });
         });
-      });
-
-    //   .then((receiver) => {
-    //     models.Event.findAll({
-    //     where: {
-    //       status: true,
-    //       userId: receiver.id,
-    //     },
-    //   });
-    // })
-    // .then(events => res.status(200).json(events));
-      // models.Event.findAll({
-      //   where: {
-      //     userId: decode.id, // on checke que l'id dans le token corresponde à la foreign key de user qui est dans event pour avoir les event d'un user en particulier
-      //     status: true,
-      //   },
-      // })
-        // .then(events => res.status(200).json(events));
-    }
+      }
+    });
   });
-});
+
+router.route('/contacts')
+  .post((req, res) => {
+    const token = getToken(req);
+    jwt.verify(token, jwtSecret, (err, decode) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        const newContact = {
+          ...req.body,
+        };
+        models.Contact.create(newContact)
+          .then((contact) => {
+            models.User.findById(decode.id)
+              .then((caregiver) => {
+                caregiver.getReceiver()
+                  .then((receivers) => {
+                    if (receivers[0].status) {
+                      receivers[0].addContact(contact)
+                        .then((contactCreated) => {
+                          res.status(200).json(contactCreated);
+                        });
+                    }
+                  });
+              });
+          });
+      }
+    });
+  })
+  .get((req, res) => {
+    const token = getToken(req);
+    jwt.verify(token, jwtSecret, (err, decode) => {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        models.User.findById(decode.id)
+        .then((caregiver) => {
+          caregiver.getReceiver()
+            .then((receivers) => {
+              if (receivers[0].status) {
+                receivers[0].getContacts()
+                  .then((contacts) => {
+                    res.status(200).json(contacts);
+                });
+              }
+          });
+        });
+      }
+    });
+  });
 
 router.route('/receivers/:caregiverId')
   .get((req, res) => {
-    const { caregiverId } = req.params;
+    const { caregiverId } = req.params; // on récupére le 1er receiver (le plus ancien créé) et je récupére ses events avec la méthode getEvents de sequelize
     models.User.findById(caregiverId).then((caregiver) => {
       caregiver.getReceiver().then((receivers) => {
         res.status(200).json(receivers);
